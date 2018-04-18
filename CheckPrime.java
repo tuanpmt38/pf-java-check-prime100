e3f55d51f67bb62419ace8acf35479d80493175f
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-18 14:24:27 +0700

    [Bài tập] Hiển thị các số nguyên tố nhỏ hơn 100

--- CheckPrime.java
+++ CheckPrime.java
@@ -1,18 +1,30 @@
 public class CheckPrime {
     public static void main(String[] args) {
         int number;
-        System.out.println("Hiển thị 100 số nguyên tố đầu tiên:");
+        System.out.println("Hiển thị 20 số nguyên tố đầu tiên:");
         for (number = 2; number < 100; number++) {
-            boolean check = true;
+            boolean isCheck = true;
             {
+                //dung vong lap while
+//            int i = 2;
+//            boolean check = true;
+//            while (i <= Math.sqrt(number)) {
+//                if (number % i == 0) {
+//                    check = false;
+//                    break;
+//                }
+//                i++;
+//            }
+
+                //dùng vòng lặp for
                 int CheckNumber = (int) Math.sqrt(number);
                 for (int i = 2; i <= CheckNumber; i++) {
                     if (number % i == 0) {
-                        check = false;
+                        isCheck = false;
                         break;
                     }
                 }
-                if (check)
+                if (isCheck)
                     System.out.println(+number);
             }
         }
